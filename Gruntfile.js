408e442e0eec3bc5ca881bfab1e28d11f37cff7a
Gil Vieira <ogilvieira@gmail.com>
2016-10-13 01:33:12 -0300

    add bower e npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,91 @@
 module.exports = function(grunt) {
+  var vendor_files = {
+      javascript: [
+        './_vendor/jquery/jquery.slim.js',
+      ],
+      stylesheet: [],
+  };
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  var project_files = {
+      javascript: [
+        '!./_site/public/assets/js/app.min.js',
+        './_site/public/assets/js/lib/jquery.js',
+        './_site/public/assets/js/lib/*.js',
+        './_site/public/assets/js/modules/*.js',
+        './_site/public/assets/js/app.js',
+      ],
+      stylesheet: [
+        './_site/public/assets/css/vendor/**.css',
+        './_site/public/assets/css/main.css',
+      ],
+  };
 
   // Project configuration.
   grunt.initConfig({
-
+    pkg: grunt.file.readJSON('package.json'),
     uglify: {
+      options: {
+        banner: '/*===================================================== \n'
+                +'= <%= pkg.siteName %> \n'
+                +'= by <%= pkg.author %> \n'
+                +'= [LAST BUILD: <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %>] \n'
+                +'=====================================================*/\n'
+      },
+      build: {
+        src: project_files.javascript,
+        dest: './_site/public/assets/js/app.min.js'
+      },
+    },
+    cssmin : {
+      options : {
+        keepSpecialComments: 0,
+        rebase: true,
+        banner: '/*===================================================== \n'
+                +'= <%= pkg.siteName %> \n'
+                +'= by <%= pkg.author %> \n'
+                +'= [LAST BUILD: <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %>] \n'
+                +'=====================================================*/\n'
+      },
       dist: {
-        src: [
-          './assets/js/vendor/jquery.mask.min.js',
-          './assets/js/vendor/owl.carousel.min.js',
-          './assets/js/app.js'
-        ],
-        dest: 'assets/js/app.min.js'
+        src: project_files.stylesheet,
+        dest: './_site/public/assets/css/app.min.css',
       }
     },
-
-    cssmin: {
+    htmlmin: {
       options: {
-        keepSpecialComments: 0
-      },      
+        removeComments: true,
+        collapseWhitespace: true
+      },
       dist: {
-        src: [
-          './assets/css/normalize.min.css',
-          './assets/css/font-awesome.min.css',
-          './assets/css/owl.carousel.css',
-          './assets/css/owl.transitions.css',
-          './assets/css/grid-1200.css',
-          './assets/css/app.css'
-        ],
-        dest: './assets/css/app.min.css'
+        src: '_site/public/index.html',
+        dest: '_site/public/index.min.html',
       }
-    }   
+    },
+     processhtml: {
+      options: {
+         process: true,
+       },
+      dist: {
+        src: '_site/public/index.min.html',
+        dest: '_site/public/index.html',
+      }
+     },
+    imagemin: {                          // Task
+      dynamic: {                         // Another target
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: '_site/public/assets/img/',                   // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: '_site/public/assets/img/'                  // Destination path prefix
+        }]
+      }
+    }
   });
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify', 'cssmin']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin', 'processhtml', 'imagemin']);
 };
